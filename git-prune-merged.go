1908714b5fa8a4d52780673d5095566cddc840a3
Piotr Banasik <piotr@jane.app>
2021-04-29 15:58:28 -0700

    Try to rebase the branch first before merging

--- git-prune-merged.go
+++ git-prune-merged.go
@@ -34,6 +34,78 @@ func gitStatusCount(repo *git.Repository) (int, error) {
 	return count, nil
 }
 
+func cleanup(repo *git.Repository, currentBranchName string) {
+	if utils.FileExists(".git/MERGE_HEAD") {
+		utils.Run("git", "merge", "--abort")
+	}
+	if utils.FileExists(".git/REBASE_HEAD") {
+		utils.Run("git", "rebase", "--abort")
+	}
+	utils.Run("git", "clean", "-fq")
+
+	currentBranch, err := gitutil.CurrentBranch(repo)
+	handleError(err)
+
+	if currentBranch.Name != currentBranchName {
+		utils.Run("git", "checkout", currentBranchName)
+	}
+}
+
+func processBranch(repo *git.Repository, branch gitutil.BranchInfo, candidates []string, currentBranchName string) []string {
+	fmt.Printf("[%v] ... ", branch.Name)
+	err := utils.RunE("git", "clean", "-fq")
+	if err != nil {
+		fmt.Print("failed to clean\n")
+		fmt.Printf("%o", err)
+		return candidates
+	}
+
+	_, err = utils.BacktickE("git", "checkout", branch.Name)
+	if err != nil {
+		fmt.Print("checkout branch failed\n")
+		cleanup(repo, currentBranchName)
+		return candidates
+	}
+
+	_, err = utils.BacktickE("git", "rebase", currentBranchName)
+	if err != nil {
+		fmt.Print("rebase branch failed\n")
+		cleanup(repo, currentBranchName)
+		return candidates
+	}
+
+	_, err = utils.BacktickE("git", "checkout", currentBranchName)
+	if err != nil {
+		fmt.Print("checkout current branch failed\n")
+		cleanup(repo, currentBranchName)
+		return candidates
+	}
+
+	_, err = utils.BacktickE("git", "merge", "--no-commit", "--no-ff", branch.Name)
+	if err != nil {
+		fmt.Print("merge failed\n")
+		cleanup(repo, currentBranchName)
+		return candidates
+	}
+
+	count, err := gitStatusCount(repo)
+	if err != nil {
+		handleError(err)
+	} else {
+		if count > 0 {
+			fmt.Print("had some extra changes\n")
+		} else {
+			fmt.Print("merged cleanly\n")
+			candidates = append(candidates, branch.Name)
+		}
+	}
+
+	// cleanup
+	cleanup(repo, currentBranchName)
+
+	return candidates
+}
+
 func main() {
 	flag.Parse()
 	candidates := []string{}
@@ -57,36 +129,7 @@ func main() {
 			continue
 		}
 
-		fmt.Printf("[%v] ... ", branch.Name)
-		err := utils.RunE("git", "clean", "-fq")
-		if err == nil {
-
-			_, err = utils.BacktickE("git", "merge", "--no-commit", "--no-ff", branch.Name)
-			if err == nil {
-				count, err := gitStatusCount(repo)
-				if err != nil {
-					handleError(err)
-				} else {
-					if count > 0 {
-						fmt.Print("had some extra changes\n")
-					} else {
-						fmt.Print("merged cleanly\n")
-						candidates = append(candidates, branch.Name)
-					}
-				}
-			} else {
-				fmt.Print("merge failed\n")
-			}
-
-			// cleanup
-			if utils.FileExists(".git/MERGE_HEAD") {
-				utils.Run("git", "merge", "--abort")
-			}
-			utils.Run("git", "clean", "-fq")
-		} else {
-			fmt.Print("failed to clean\n")
-			fmt.Printf("%o", err)
-		}
+		candidates = processBranch(repo, branch, candidates, currentBranch.Name)
 	}
 
 	if len(candidates) == 0 {
